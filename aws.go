7929741b282ec12446dd1e86f35d894265c2b984
ssuareza <sebastian@typeform.com>
2019-11-22 12:08:54 +0100

    Adding homebrew tap

--- aws.go
+++ aws.go
@@ -6,6 +6,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go/service/ec2/ec2iface"
 )
 
 // Get gets the instances from aws
@@ -19,12 +20,8 @@ func Get(profile string, region string) ([]map[string]string, error) {
 		SharedConfigState: session.SharedConfigEnable,
 	})
 
-	// create service
-	svc := ec2.New(sess)
-
-	// get instances
 	// Only grab instances that are running or just started
-	params := &ec2.DescribeInstancesInput{
+	input := &ec2.DescribeInstancesInput{
 		Filters: []*ec2.Filter{
 			&ec2.Filter{
 				Name: aws.String("instance-state-name"),
@@ -36,9 +33,25 @@ func Get(profile string, region string) ([]map[string]string, error) {
 		},
 	}
 
-	res, _ := svc.DescribeInstances(params)
+	// create service
+	svc := ec2.New(sess)
+
+	// this is for testing
+	StubEC2(svc, input)
+
+	// get instances
+	res, _ := svc.DescribeInstances(input)
 
 	return Sort(res), err
+}
+
+// stub (this is for testing)
+func StubEC2(svc ec2iface.EC2API, input *ec2.DescribeInstancesInput) (*ec2.DescribeInstancesOutput, error) {
+	res, err := svc.DescribeInstances(input)
+	if err != nil {
+		return nil, err
+	}
+	return res, err
 }
 
 func nilGuard(ptr *string) string {
